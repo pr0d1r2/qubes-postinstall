2a6baf292bc1070ede792f1f04df7516da12a48a
Marcin Nowicki <pr0d1r2@gmail.com>
2016-01-10 14:36:27 +0100

    Make install work

--- install_whonix.sh
+++ install_whonix.sh
@@ -4,14 +4,14 @@ source `dirname $0`/misc.sh
 
 run_once sudo qubes-dom0-update --enablerepo=qubes-templates-community qubes-template-whonix-gw qubes-template-whonix-ws
 
-run_once qvm-create sys-whonix --hvm --label red --template=whonix-gw --proxy
-run_once qvm-create anon-whonix --hvm --label red --template=whonix-ws
+run_once qvm-create --label red --template=whonix-gw --proxy sys-whonix
+run_once qvm-create --label red --template=whonix-ws anon-whonix
 
-run_once qvm-prefs sys-whonix -set netvm=sys-firewall
-run_once qvm-prefs anon-whonix -set netvm=sys-whonix
+run_once qvm-prefs -s sys-whonix netvm sys-firewall
+run_once qvm-prefs -s anon-whonix netvm sys-whonix
 
-run_once qvm-prefs whonix-gw -set netvm=sys-whonix
-run_once qvm-prefs whonix-ws -set netvm=sys-whonix
+run_once qvm-prefs -s whonix-gw netvm sys-whonix
+run_once qvm-prefs -s whonix-ws netvm sys-whonix
 
 run_once qvm-start sys-whonix
 run_once qvm-start anon-whonix
