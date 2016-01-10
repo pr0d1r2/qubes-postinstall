8d6101005255776fd36caa15480f447dfdd0b8b3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-01-10 14:25:28 +0100

    Adjust alias to support paths

--- misc.sh
+++ misc.sh
@@ -2,7 +2,7 @@ function run_once() {
   if [ ! -d ~/.run_once ]; then
     mkdir ~/.run_once
   fi
-  local run_once_MARKER=~/.run_once/`echo $@ | tr ' ' '_'`
+  local run_once_MARKER=~/.run_once/`echo $@ | tr ' ' '_' | tr '/' '_'`
   if [ ! -f $run_once_MARKER ]; then
     $@ || return $?
     touch $run_once_MARKER
